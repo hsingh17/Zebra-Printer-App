ab0b7fd6a9a4bc4b304135cbb8332c203242972a
Harjot Singh <hlsingh@ucdavis.edu>
2022-01-03 21:35:29 -0800

    Fix path to label_db.json in server.js

--- zebra_printer_webapp/server.js
+++ zebra_printer_webapp/server.js
@@ -3,9 +3,9 @@ const app = express()
 const path = require('path')
 const fs = require('fs/promises')
 const PORT = process.env.PORT || 3000
+const label_db_path = path.join(__dirname, 'label_db.json')
 
 // In case localhost doesn't work: https://stackoverflow.com/questions/58929202/connection-refused-on-localhost
-
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({
@@ -13,20 +13,20 @@ app.use(express.urlencoded({
 })) // https://flaviocopes.com/express-forms/
 
 app.post('/label', async (req, res) => {
-    let data = await fs.readFile('label_db.json')
+    let data = await fs.readFile(label_db_path)
     let db = JSON.parse(data)
     db.push(req.body)   // Push in the new label request
 
     let new_data = JSON.stringify(db)
-    await fs.writeFile('label_db.json', new_data)
+    await fs.writeFile(label_db_path, new_data)
     res.status(204).send()  // 204 = No Content Response
 })
 
 app.get('/label', async (req, res) => {
-    let data = await fs.readFile('label_db.json')
+    let data = await fs.readFile(label_db_path)
     let db = JSON.parse(data)
 
-    await fs.writeFile('label_db.json', JSON.stringify([])) // Clear the old values
+    await fs.writeFile(label_db_path, JSON.stringify([])) // Clear the old values
     res.json(db)
 })
 
